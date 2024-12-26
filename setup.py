578fa593d8ddf306942176e102925da5036db3d6
Black-Lights <mohammadammar.mughees@mail.polimi.it>
2024-12-26 19:35:58 +0100

    integrated required dependencies in setup file

--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
-
 from setuptools import setup, find_packages
 
 setup(
-    name='histogram_manipulation',
-    version='0.1.0',
-    packages=find_packages(),
+    name='histogram_manipulation',              # Name of your package
+    version='0.1.0',                            # Initial version
+    packages=find_packages(),                   # Automatically find all packages
     install_requires=[
-        'numpy',
-        'opencv-python',
-        'matplotlib',
-        'rasterio',
-        'scikit-image',
+        'numpy>=1.21',                          # Specify minimum versions if needed
+        'opencv-python>=4.5',
+        'matplotlib>=3.0',
+        'rasterio>=1.2',
+        'scikit-image>=0.18',
     ],
-    author='EA',
+    author='EA',                                # Author name
+    author_email='your.email@example.com',      # Optional email
     description='A library for histogram manipulation of images',
-    url='https://github.com/Black-Lights/hist_man.git',
+    long_description=open('README.md').read(),  # Use README as the description
+    long_description_content_type='text/markdown',  # Specify markdown format
+    url='https://github.com/Black-Lights/hist_man.git',  # GitHub repo URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',                    # Specify compatible Python versions
 )
